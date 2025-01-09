88126b054c00f5b1bc808bbc9fd3ff7eaa32b849
Nicky Sandhu <psandhu@water.ca.gov>
2025-01-08 17:02:21 -0800

    fix: reduce number of processes for panel server to improve resource usage

--- run_server.sh
+++ run_server.sh
@@ -4,4 +4,4 @@ if [ -f "caching.log" ]; then
 else
     python repocache.py $dir > 'caching.log' 2>&1 &
 fi
-panel serve repoui.py --num-procs 2 --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
+panel serve repoui.py --num-procs 1 --address 0.0.0.0 --port 80 --allow-websocket-origin="*"
